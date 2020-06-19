87eb1ea554e9e650a80fd1babbab15c307c9fea3
for-hk <53420891+for-hk@users.noreply.github.com>
2020-06-19 18:28:04 +0800

    update to use api

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -1,5 +1,6 @@
 class RubyGemsController < ApplicationController
-
+  protect_from_forgery unless: -> { request.format.json? }
+  
   PAGE_SIZE = 30
 
   def index
@@ -10,6 +11,31 @@ class RubyGemsController < ApplicationController
       page_offset = (@current_page - 1) * PAGE_SIZE
 
       @search_results = RubyGem.order(:created_at).limit(PAGE_SIZE).offset(page_offset)
+    end
+  end
+
+  def search
+    require "uri"
+    require "net/http"
+    require "json"
+
+    url = URI("https://h72ashmv3j.execute-api.ap-northeast-1.amazonaws.com/dev/v1/licensed_sponsors/query")
+
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "text/plain"
+    request.body = "{\"name\":\""+search_params[:q]+"\"}"
+
+    response = https.request(request)
+    # puts response.read_body
+    @results = JSON.parse(response.read_body)['result']
+    
+
+    # @selected = Item.where(:category_id => params[:cat_id])
+    respond_to do |format|
+        format.js
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :ruby_gems, only: [:index, :show]
-
+  post '/search', to: 'ruby_gems#search'
   root 'ruby_gems#index'
+
 end
